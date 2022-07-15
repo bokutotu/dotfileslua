7f385497ff87a0a8ff75abfc275fb4674a1a8a98
bokutotu <mushin.hudoushin@gmail.com>
2022-07-15 18:08:16 +0900

    add nvim settings

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -30,14 +30,52 @@ return require('packer').startup({function()
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-
-  -- lsp
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-
+  
   -- fizzy finder
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-frecency.nvim'
 
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'onsails/lspkind-nvim'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-nvim-lsp-document-symbol'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'f3fora/cmp-spell'
+  use 'yutkat/cmp-mocword'
+  use 'ray-x/cmp-treesitter'
+  -- use 'tami5/lspsaga.nvim'
+  use 'folke/lsp-colors.nvim'
+  use 'folke/trouble.nvim'
+  use 'lukas-reineke/cmp-under-comparator'
+  use 'kkharji/lspsaga.nvim' 
+
+  -- rust
+  use 'simrat39/rust-tools.nvim'
+
+  -- treesitter
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'yioneko/nvim-yati'
+  
+  -- 括弧
+  use 'andymass/vim-matchup'
+  use 'jiangmiao/auto-pairs'
+
+  -- snipet
+  use 'L3MON4D3/LuaSnip'
+
+  -- コメント
+  use 'numToStr/Comment.nvim'
+
+  -- MarkDown
+  use 'iamcco/markdown-preview.nvim'
 
 end, config = {compile_path = util.join_paths(vim.fn.stdpath('config'), 'packer_compiled.vim')}});
