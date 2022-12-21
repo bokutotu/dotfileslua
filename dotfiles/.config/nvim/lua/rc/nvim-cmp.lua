848806efb25a998fc9bf47bd48a97c68d12f267c
bokutotu <mushin.hudoushin@gmail.com>
2022-12-21 15:38:41 +0900

    update

--- dotfiles/.config/nvim/lua/rc/nvim-cmp.lua
+++ dotfiles/.config/nvim/lua/rc/nvim-cmp.lua
@@ -215,7 +215,7 @@ cmp.setup.cmdline(":", {
 })
 
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lsp_attach = function(client, buf)
 	-- Example maps, set your own with vim.api.nvim_buf_set_keymap(buf, "n", <lhs>, <rhs>, { desc = <desc> })
 	-- or a plugin like which-key.nvim
