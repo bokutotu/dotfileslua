12dbd2b482d089b960b96c62e41f0df946110c89
bokutotu <mushin.hudoushin@gmail.com>
2023-02-11 12:12:01 +0900

    update for MAUI

--- dotfiles/.config/nvim/lua/indent.lua
+++ dotfiles/.config/nvim/lua/indent.lua
@@ -4,6 +4,8 @@ cmd [[
 augroup fileTypeIndent
     autocmd!
     autocmd BufNewFile,BufRead *.py setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.cs setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.xaml setlocal tabstop=4 softtabstop=4 shiftwidth=4
     autocmd BufNewFile,BufRead *.lua setlocal tabstop=2 softtabstop=2 shiftwidth=2
     autocmd BufNewFile,BufRead *.cu setlocal tabstop=2 softtabstop=2 shiftwidth=2
     autocmd BufNewFile,BufRead *.c  setlocal tabstop=2 softtabstop=2 shiftwidth=2
