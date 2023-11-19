f1adf0fab6404687aa0a73e6d82cb122662598b9
bokutotu <mushin.hudoushin@gmail.com>
2023-11-19 19:40:35 +0900

    telescopeでC-n, C-pで移動できるようにした

--- dotfiles/.config/nvim/lua/rc/telescope.lua
+++ dotfiles/.config/nvim/lua/rc/telescope.lua
@@ -1,3 +1,18 @@
 require('which-key').register({
 ['<leader>ff'] = {'<Cmd>Telescope git_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<CR>', 'telescope: find file'},
 })
+
+-- Telescope key mappings
+local actions = require('telescope.actions')
+
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.move_selection_next,
+        ["<C-p>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
+
