e7cfeee75a2b8809201a99d3e651ae3fa42d3c53
bokutotu <mushin.hudoushin@gmail.com>
2023-11-21 17:19:03 +0900

    dartだけtree-sitterのindent機能を無効化する

--- dotfiles/.config/nvim/lua/rc/treesitter.lua
+++ dotfiles/.config/nvim/lua/rc/treesitter.lua
@@ -1,7 +1,7 @@
 -- Tree-sitterの設定
 require'nvim-treesitter.configs'.setup {
   -- ここで有効にする機能を指定します
-  ensure_installed = {"lua","rust", "python", "dart", "c", "cpp"}, -- インストールする言語パーサを指定
+  ensure_installed = {"lua","rust", "python", "c", "cpp"}, -- インストールする言語パーサを指定
   sync_install = false, -- 起動時にインストールを同期的に実行するかどうか
   auto_install = true, -- 言語パーサがない場合に自動的にインストールするかどうか
   highlight = {
@@ -12,4 +12,19 @@ require'nvim-treesitter.configs'.setup {
     enable = true, -- インデントをTree-sitterで行うかどうか
   },
 }
+
+-- dartのTree-sitterのindentが遅いので無効化する
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "dart",
+    callback = function()
+        vim.cmd("TSDisable indent")
+    end
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+    pattern = "*.dart",
+    callback = function()
+        vim.cmd("TSEnable indent")
+    end
+})
 
