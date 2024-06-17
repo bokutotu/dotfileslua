4d2081f5a45207dc59f609b1ffcd82999497a4ba
bokutotu <mushin.hudoushin@gmail.com>
2024-06-17 23:49:56 +0900

    hoge

--- dotfiles/.config/nvim/lua/rc/trobule.lua
+++ dotfiles/.config/nvim/lua/rc/trobule.lua
@@ -46,25 +46,28 @@ require('trouble').setup({
     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 })
 
--- Lua
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<F11>", "<cmd>Trouble lsp_references<cr>",
-  {silent = true, noremap = true}
-)
-vim.api.nvim_set_keymap("n", "<F12>", "<cmd>Trouble lsp_definitions<cr>",
-  {silent = true, noremap = true}
-)
+-- -- Lua
+-- vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<F11>", "<cmd>Trouble lsp_references<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "<F12>", "<cmd>Trouble lsp_definitions<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
