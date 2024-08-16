f9e8a6867149c65a929116432568795b16ce6163
bokutotu <mushin.hudoushin@gmail.com>
2024-08-16 16:15:12 +0000

    update init.bash

--- init.bash
+++ init.bash
@@ -20,4 +20,27 @@ source $HOME/.cargo/env
 
 cargo run --release
 
+cargo install lsd
+
 source ~/.zshrc
+
+  git config --global user.email "mushin.hudoushin@gmail.com"
+  git config --global user.name "bokutotu"
+
+# git clone neovim
+apt install cmake -y
+apt install build-essential -y
+apt install gettext -y
+git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo && make install
+
+# inistall tmux 
+apt install tmux -y
+
+# install deno
+ apt install unzip -y
+# denoをインストールする
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+echo 'export DENO_INSTALL="$HOME/.deno"' >> ~/.alias.zsh
+echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> ~/.alias.zsh
