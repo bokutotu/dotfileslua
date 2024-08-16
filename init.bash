cf3ddf2a42681fce6752627094cc294eb0a1ff4e
bokutotu <mushin.hudoushin@gmail.com>
2024-08-16 15:37:45 +0000

    add init.bash

---
+++ init.bash
@@ -0,0 +1,23 @@
+#!bin/bash
+
+# update upgrae
+apt update && apt upgrade
+
+# install zsh
+sudo apt install zsh -y
+
+chsh -s $(which zsh)
+
+# Rustのインストール
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# ~/.alias.zshにCargoのパスを追加
+echo '' >> ~/.alias.zsh
+echo '# Rust/Cargo環境変数' >> ~/.alias.zsh
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.alias.zsh
+
+source $HOME/.cargo/env
+
+cargo run --release
+
+source ~/.zshrc
