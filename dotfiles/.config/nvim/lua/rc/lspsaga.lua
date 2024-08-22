fb2be8b743cc6eaf247494e8b9476afa5d8145fb
bokutotu <mushin.hudoushin@gmail.com>
2024-08-22 22:26:19 +0900

    update copilot chat

--- dotfiles/.config/nvim/lua/rc/lspsaga.lua
+++ dotfiles/.config/nvim/lua/rc/lspsaga.lua
@@ -71,8 +71,8 @@ local map = vim.api.nvim_buf_set_keymap
 --- map(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true})
 --- map(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true})
 --- map(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true})
-map(0, "n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
-map(0, "n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
+-- map(0, "n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
+-- map(0, "n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
 -- Function to set keymaps
 local function map(mode, lhs, rhs, opts)
     local options = { noremap = true, silent = true }
