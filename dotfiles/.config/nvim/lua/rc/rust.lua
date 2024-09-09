90cd0e85f232fe9f706a11ab1c5c3de1cfb8cafe
bokutotu <mushin.hudoushin@gmail.com>
2024-09-10 02:00:05 +0900

    update rust

--- dotfiles/.config/nvim/lua/rc/rust.lua
+++ dotfiles/.config/nvim/lua/rc/rust.lua
@@ -6,10 +6,90 @@ end
 
 map('n', '<leader>rd', '<cmd>RustLsp renderDiagnostic current<CR>', { desc = 'rust: render diagnostic' })
 
--- Run cargo fmt on save
-vim.api.nvim_exec([[
-  augroup RustFmt
-    autocmd!
-    autocmd BufWritePre *.rs :silent! lua vim.lsp.buf.formatting_sync(nil, 100)
-  augroup END
-]], false)
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- vim.g.rustaceanvim = {
+--   tools = {
+--     -- Automatically run clippy checks on save
+--     enable_clippy = true,
+--     -- Enable hover actions
+--     hover_actions = {
+--       auto_focus = true,
+--     },
+--   },
+--   server = {
+--     settings = {
+--       ["rust-analyzer"] = {
+--         checkOnSave = {
+--           command = "clippy",
+--           extraArgs = { "--", "-W", "clippy::pedantic" },
+--         },
+--       },
+--     },
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   },
+-- }
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local util = require("lspconfig.util")
+
+vim.g.rustaceanvim = {
+  tools = {
+    -- Automatically run clippy checks on save
+    enable_clippy = true,
+    -- Enable hover actions
+    hover_actions = {
+      auto_focus = true,
+    },
+    inlay_hints = {
+      auto = true,
+      only_current_line = false,
+      show_parameter_hints = true,
+    },
+    executor = require("rustaceanvim/executors").termopen,
+    reload_workspace_from_cargo_toml = true,
+  },
+  server = {
+    standalone = true, -- This allows rustaceanvim to manage its own rust-analyzer instance
+    on_attach = on_attach,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+          extraArgs = { "--", "-W", "clippy::pedantic" },
+        },
+        cargo = {
+          allFeatures = true,
+        },
+        -- Add more rust-analyzer settings as needed
+      },
+    },
+    capabilities = capabilities,
+    root_dir = function(fname)
+      local function is_library(path)
+        local cargo_home = os.getenv("CARGO_HOME") or util.path.join(vim.env.HOME, ".cargo")
+        local registry = util.path.join(cargo_home, "registry", "src")
+        local git_registry = util.path.join(cargo_home, "git", "checkouts")
+        local rustup_home = os.getenv("RUSTUP_HOME") or util.path.join(vim.env.HOME, ".rustup")
+        local toolchains = util.path.join(rustup_home, "toolchains")
+        
+        for _, item in ipairs({ toolchains, registry, git_registry }) do
+          if util.path.is_descendant(item, path) then
+            return true
+          end
+        end
+        return false
+      end
+
+      if is_library(fname) then
+        return nil
+      end
+
+      return util.root_pattern("Cargo.toml", "rust-project.json")(fname)
+        or util.find_git_ancestor(fname)
+    end,
+  },
+}
