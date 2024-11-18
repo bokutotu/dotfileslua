01fe26e1daafa2739247c97d288a0249ee371b91
bokutotu <mushin.hudoushin@gmail.com>
2024-11-18 16:57:33 +0900

    add ts settings

--- dotfiles/.config/nvim/lua/rc/lsp.lua
+++ dotfiles/.config/nvim/lua/rc/lsp.lua
@@ -32,6 +32,18 @@ lspconfig.ccls.setup {
   filetypes = { "cuda", "cpp", "c" }
 }
 
+local ts_ls_path = vim.fn.system("which typescript-language-server"):gsub("%s+", "") -- Trim whitespace
+
+-- Adding configuration for ts_ls
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  settings = {
+    -- Add any specific settings here, if needed, for ts_ls
+  },
+  filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }, -- Adjust as needed for your needs
+  cmd = { ts_ls_path, "--stdio" } -- Use the dynamically determined path
+}
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
