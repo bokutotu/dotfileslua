2309c51918eff3350852e55b817b8479a3d9f517
bokutotu <mushin.hudoushin@gmail.com>
2024-12-17 18:46:36 +0900

    rustfmtの設定を変更した

--- dotfiles/.config/nvim/lua/rc/neoformat.lua
+++ dotfiles/.config/nvim/lua/rc/neoformat.lua
@@ -1,3 +1,10 @@
+-- Neoformatでrustfmtにエディション2021を指定
+vim.g.neoformat_rust_rustfmt = {
+    exe = "rustfmt",
+    args = {"--edition", "2021"},
+    stdin = 1
+}
+
 -- Rust用の自動フォーマット設定
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.rs",
@@ -7,6 +14,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("rust_format", { clear = true }),
 })
 
+-- Neoformatの詳細な出力を有効にする
+-- vim.g.neoformat_verbose = 1
+
 -- Rustに対して有効なフォーマッタをrustfmtに設定
 vim.g.neoformat_enabled_rust = { "rustfmt" }
-
