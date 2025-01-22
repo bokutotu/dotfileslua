51738ba6c7985ba1767d730dc5cd697bda7c5d64
bokutotu <mushin.hudoushin@gmail.com>
2025-01-23 00:21:55 +0900

    add deepseek r1

--- dotfiles/.config/nvim/lua/rc/avante.lua
+++ dotfiles/.config/nvim/lua/rc/avante.lua
@@ -1,5 +1,13 @@
 require("avante").setup({
-  provider="copilot",
+  provider = "deepseek",
+  vendors = {
+    deepseek = {
+      __inherited_from = "openai",
+      api_key_name = "default",
+      endpoint = "https://api.deepseek.com",
+      model = "deepseek-reasoner",
+    },
+  },
 })
 require("avante_lib").load()
 require("copilot").setup()
