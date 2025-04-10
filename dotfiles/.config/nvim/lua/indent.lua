46ab7074a7623e395c2581202fc2d3ed6bcbe826
bokutotu <mushin.hudoushin@gmail.com>
2025-04-10 18:44:08 +0900

    tsのインデントを変更

--- dotfiles/.config/nvim/lua/indent.lua
+++ dotfiles/.config/nvim/lua/indent.lua
@@ -19,10 +19,10 @@ augroup fileTypeIndent
     autocmd BufNewFile,BufRead *.rb setlocal tabstop=2 softtabstop=2 shiftwidth=2
     autocmd BufNewFile,BufRead *.elm setlocal tabstop=4 softtabstop=4 shiftwidth=4
 
-    autocmd BufRead,BufNewFile *.js setlocal tabstop=2 softtabstop=2 shiftwidth=2
+    autocmd BufRead,BufNewFile *.js setlocal tabstop=4 softtabstop=4 shiftwidth=4
     autocmd BufRead,BufNewFile *.jsx setlocal tabstop=2 softtabstop=2 shiftwidth=2
     autocmd BufRead,BufNewFile *.tsx setlocal tabstop=2 softtabstop=2 shiftwidth=2
-    autocmd BufRead,BufNewFile *.ts setlocal tabstop=2 softtabstop=2 shiftwidth=2
+    autocmd BufRead,BufNewFile *.ts setlocal tabstop=4 softtabstop=4 shiftwidth=4
 
     autocmd BufRead,BufNewFile *.html setlocal tabstop=2 softtabstop=2 shiftwidth=2
     autocmd BufRead,BufNewFile *.css setlocal tabstop=2 softtabstop=2 shiftwidth=2
