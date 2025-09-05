4ef5be7588709b1762f20eb06fa76ba93749ba89
bokutotu <mushin.hudoushin@gmail.com>
2025-09-06 00:31:30 +0900

    cuh indent 4

--- dotfiles/.config/nvim/lua/indent.lua
+++ dotfiles/.config/nvim/lua/indent.lua
@@ -15,6 +15,7 @@ augroup fileTypeIndent
     autocmd BufNewFile,BufRead *.nix setlocal tabstop=2 softtabstop=2 shiftwidth=2
 
     autocmd BufNewFile,BufRead *.cu setlocal tabstop=4 softtabstop=4 shiftwidth=4
+    autocmd BufNewFile,BufRead *.cuh setlocal tabstop=4 softtabstop=4 shiftwidth=4
     autocmd BufNewFile,BufRead *.c  setlocal tabstop=4 softtabstop=4 shiftwidth=4
     autocmd BufNewFile,BufRead *.h  setlocal tabstop=4 softtabstop=4 shiftwidth=4
     autocmd BufNewFile,BufRead *.cpp  setlocal tabstop=4 softtabstop=4 shiftwidth=4
